e063c0a6651a9198d33b9bb3ae845f30b721a40b
Subhadip Bag <subhadipbag906@gmail.com>
2025-05-26 17:19:23 +0530

    adding limiter

---
+++ backend/middlewares/rateLimiter.js
@@ -0,0 +1,33 @@
+const requestCounts = {};
+
+//rate limiter
+const rateLimiter = (req, res, next) => {
+  const ip = req.ip;
+  const now = Date.now();
+
+  if (!requestCounts[ip]) {
+    requestCounts[ip] = { count: 1, lastRequest: now };
+  } else {
+    const timeSinceLastRequest = now - requestCounts[ip].lastRequest;
+    const timeLimit = 60 * 1000; // 1 minutes
+
+    if (timeSinceLastRequest < timeLimit) {
+      requestCounts[ip].count += 1;
+    } else {
+      requestCounts[ip] = { count: 1, lastRequest: now };
+    }
+  }
+
+  const maxRequests = 10;
+
+  if (requestCounts[ip].count > maxRequests) {
+    return res
+      .status(429)
+      .json({ message: "Too many requests, please try again later." });
+  }
+
+  requestCounts[ip].lastRequest = now;
+  next();
+};
+
+module.exports = { rateLimiter };
